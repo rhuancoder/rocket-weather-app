d3920cebedab79df978b0561eedd1de6fbc514bf
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-01 17:32:17 -0300

    feat: add new project

---
+++ Rocket Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rocket Weather
+//
+//  Created by Rhuan Carvalho on 01/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
