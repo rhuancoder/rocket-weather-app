96df3b3c044393af2f551f67f08bb0d8d0e87360
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-07 10:05:50 -0300

    feat: add hourly forecast

---
+++ Rocket Weather/Image+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Image+Extensions.swift
+//  Rocket Weather
+//
+//  Created by Rhuan Carvalho on 06/02/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static let sunIcon = UIImage(named: "sunIcon")
+}
