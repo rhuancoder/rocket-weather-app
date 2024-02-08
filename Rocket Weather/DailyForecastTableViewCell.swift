85828470ed599b267e9d3e434dd3816f30964fba
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-08 14:56:04 -0300

    feat: add daily forecast

---
+++ Rocket Weather/DailyForecastTableViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  DailyForecastTableViewCell.swift
+//  Rocket Weather
+//
+//  Created by Rhuan Carvalho on 07/02/24.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "DailyForecast"
+    
+    private lazy var weekDayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "WED"
+        label.textColor = UIColor.contrast
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var minTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "min 11°C"
+        label.textColor = UIColor.contrast
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var maxTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "max 25°C"
+        label.textColor = UIColor.contrast
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage.cloudIcon
+        return imageView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [weekDayLabel, iconImageView, minTemperatureLabel, maxTemperatureLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        stackView.spacing = 15
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        contentView.addSubview(stackView)
+    }
+    
+    private func setConstraints() {
+        stackView.setConstraintsToParent(contentView)
+        
+        NSLayoutConstraint.activate([
+            weekDayLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 50)
+        ])
+    }
+}
