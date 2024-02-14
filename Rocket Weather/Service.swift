7408d99b627ef3a506414d45694329ec6003e34f
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-02-14 10:59:49 -0300

    refactor: change view and service

--- Rocket Weather/Service.swift
+++ Rocket Weather/Service.swift
@@ -43,21 +43,19 @@ class Service {
     }
 }
 
-// MARK: - ForecastResponse
 struct ForecastResponse: Codable {
     let current: Forecast
     let hourly: [Forecast]
     let daily: [DailyForecast]
 }
 
-// MARK: - Forecast
 struct Forecast: Codable {
     let dt: Int
     let temp: Double
     let humidity: Int
     let windSpeed: Double
     let weather: [Weather]
-
+    
     enum CodingKeys: String, CodingKey {
         case dt, temp, humidity
         case windSpeed = "wind_speed"
@@ -65,20 +63,17 @@ struct Forecast: Codable {
     }
 }
 
-// MARK: - Weather
 struct Weather: Codable {
     let id: Int
     let main, description, icon: String
 }
 
-// MARK: - DailyForecast
 struct DailyForecast: Codable {
     let dt: Int
     let temp: Temp
     let weather: [Weather]
 }
 
-// MARK: - Temp
 struct Temp: Codable {
     let day, min, max, night: Double
     let eve, morn: Double
